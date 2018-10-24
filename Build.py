41b45d3bc5a30085fbecb2084ce0f70bcb9aa582
Luiz Fernando de Lima <luizfernandofpl@gmail.com>
2018-10-23 22:43:18 -0300

    Finalização da implementação da construção da solução inicial

--- Build.py
+++ Build.py
@@ -17,13 +17,14 @@ def readFile():
 
 	return conteudoArq
 
-def build():
-	conteudoArq = readFile()
-
+def graphRep(conteudoArq):
 	print(conteudoArq)
 
 	vertices = []
 	nConexoes = []
+	adjacentes = [[]] #começa com 1 elemento/lista vazia que vai ser eliminada depois da primeira iteração
+
+	cont = 1
 
 	for ele in conteudoArq:
 		auxList = ele.split(' ')
@@ -40,17 +41,29 @@ def build():
 			indice = vertices.index(vertice1)
 			nConexoes[indice] += 1
 
+			adjacentes[indice].append(vertice2)
+
 		else:
 			vertices.append(vertice1)
 			nConexoes.append(1)
+
+			adjacentes.append(list(vertice2))
 
 		if vertice2 in vertices:
 			indice = vertices.index(vertice2)
 			nConexoes[indice] += 1
 
+			adjacentes[indice].append(vertice1)
+
 		else:
 			vertices.append(vertice2)
 			nConexoes.append(1)
+
+			adjacentes.append(list(vertice1))
+
+		if cont == 1: #pra remover o primeiro elemento desnecessário
+			adjacentes.pop(0)
+			cont -= 1
 		
 		#print(vertices)
 
@@ -61,3 +74,64 @@ def build():
 		print('Vértice:', vertices[i])
 		print('Num Conexões:', nConexoes[i])
 		print('\n')
+
+	#print(arestas)
+
+	return vertices, nConexoes, adjacentes
+
+
+def getColourSet():
+	#colourDict = ['red','green','blue','yellow','black','gray','pink','violet']
+
+	colourSet = ['R','G','B','Y','BK','GR','P','V']
+
+	return colourSet
+
+def solution0(vertices, nConexoes, adjacentes):
+	colourSet = getColourSet()
+
+	print(colourSet)
+
+	cores = ['']*len(vertices)
+
+	pair = zip(nConexoes, vertices)
+	ordemVertices = [x for y, x in sorted(pair, reverse = True)]
+	print('\n')
+	print(ordemVertices)
+
+	ordemIndices = []
+
+	for i in ordemVertices:
+		ordemIndices.append(ordemVertices.index(i))
+
+	for i in ordemIndices:
+		
+		for c in colourSet:
+			
+			for adj in adjacentes[i]:
+				
+				indiceAdj = vertices.index(adj)
+
+				if c == cores[indiceAdj]:
+					canUse = False
+					break
+				else:
+					canUse = True
+
+			if canUse:
+				cores[i] = c
+				break
+
+	return cores
+
+
+def build():
+	conteudoArq = readFile()
+
+	vertices, nConexoes, adjacentes = graphRep(conteudoArq)
+
+	print(adjacentes)
+
+	cores = solution0(vertices, nConexoes, adjacentes)
+
+	print(cores)
